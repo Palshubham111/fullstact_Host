f34eb70116e4f7c611dd0dd6fe67caa4ce081b87
palshubham111 <shubhampal0025@gmail.com>
2025-09-23 16:12:21 +0530

    add new

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
 
   useEffect(() =>{
 
-    fetch("https://fullstact-host.vercel.app/")
+    fetch("https://fullstact-host.vercel.app//")
   .then((res) => res.json())
   .then((data) => 
     
